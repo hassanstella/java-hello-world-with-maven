838aeb08ff8be2fe66e4406db6f2c9efe6b84d53
hassanstella <134918270+hassanstella@users.noreply.github.com>
2023-06-01 15:57:59 +0500

    Update dockerbuid.sh

--- dockerbuid.sh
+++ dockerbuid.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 IMAGE=hassan1989/testapp
 docker build -f Dockerfile -t ${IMAGE}:bamboo.buildNumber .
+docker login --username=hassan1989 --password=$Hssanm@123 
+docker push ${IMAGE}:bamboo.buildNumber 
